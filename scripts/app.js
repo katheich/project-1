c4d0225397b8a2454ccf93f1773578a6d522392a
Kathrin Eichinger <kathrin801@gmail.com>
2019-11-04 15:59:21 +0000

    monday evening

--- scripts/app.js
+++ scripts/app.js
@@ -1,43 +1,59 @@
-function setupGame() {
+function main() {
 
   // VARIABLES
-  const start = document.getElementById('start')
   const width = 21
   const grid = document.querySelector('.grid')
   const scoreCounter = document.getElementById('score')
   const lifeCounter = document.getElementById('lives')
+  const messageScreen = document.querySelector('.message')
 
-  const board = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 3, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 4, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 4, 4, 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 4, 4, 4, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-  let player = 178
-  let ghost = 253
-  const cells = [] 
+  let cells = [] 
   let score = 0
   let lives = 3
-  let ghostHistory = [0, 0]
-  let frightened = false
+  let ghostInterval
+  let energizerTimeout
 
-  
-  // SET UP GAME BOARD
-  for (let i = 0; i < width ** 2; i++){
-    const cell = document.createElement('div')
+  let player
+  let ghostHistories = []
+  let frightened 
+  let foodCount
 
-    const cellClass = board[i] 
+  // STARTING SCREEN 
+  const logo = document.createElement('img')
+  logo.setAttribute('src', 'images/cat-logo.png')
+  messageScreen.appendChild(logo)
 
-    switch (cellClass) {
-      case 1: cell.classList.add('wall'); break
-      case 2: cell.classList.add('food'); break
-      case 3: cell.classList.add('energizer'); break
-      case 4: cell.classList.add('ghostpen'); break
+  const button = document.createElement('button')
+  button.innerHTML = 'Start Game'
+  button.addEventListener('click', runGame)
+  messageScreen.appendChild(button)
+
+
+  // END GAME FUNCTION
+  function endGame(state) {
+
+    clearInterval(ghostInterval)
+    grid.innerHTML = ''
+
+    grid.appendChild(messageScreen)
+
+    if (state === 'win') {
+      messageScreen.innerHTML = `You won! Your score was ${score}`
+    } else if (state === 'lose') {
+      messageScreen.innerHTML = `Game-over! Your score was ${score}`
     }
 
-    grid.appendChild(cell)
-    cells.push(cell)
+    const logo = document.createElement('img')
+    logo.setAttribute('src', 'images/cat-logo.png')
+    messageScreen.appendChild(logo)
 
-    // cell.innerHTML = i
-  }  
+    const button = document.createElement('button')
+    button.innerHTML = 'Play again'
+    button.addEventListener('click', runGame)
+    messageScreen.appendChild(button)
 
-  cells[player].classList.add('player')
-  cells[ghost].classList.add('ghost')
+    cells = []
+  }
 
   // GET X AND Y COORDINATE OF ANY CELL
   function getXY(position) {
@@ -58,7 +74,7 @@ function setupGame() {
   }
 
   // GHOST HITS PLAYER 
-  function collideWithGhost() {
+  function collideWithGhost(ghostHistory) {
 
     console.log('COLLISION')
 
@@ -67,19 +83,22 @@ function setupGame() {
       lifeCounter.innerHTML = lives
 
       if (lives === 0) {
-        console.log(`GAME OVER, TOTAL SCORE: ${score}`)
-      }
+        endGame('lose')
 
-      cells[player].classList.remove('player')
-      player = 178
-      cells[player].classList.add('player')
+      } else {
+        cells[player].classList.remove('player')
+        player = 178
+        cells[player].classList.add('player')
+        return ghostHistory
+      }
 
     } else {
       score += 100
       
-      cells[ghost].classList.remove('ghost')
-      ghost = 219
-      cells[ghost].classList.add('ghost')
+      ghostHistory = [241, ghostHistory[0]]
+      cells[ghostHistory[1]].classList.remove('ghost')
+      cells[ghostHistory[0]].classList.add('ghost')
+      return ghostHistory
     }
   }
 
@@ -91,7 +110,9 @@ function setupGame() {
   // new ghost position: newGhost (index in array ghostOptions)
   // tracking history of ghost position: ghostHistory
   
-  function moveGhost() {
+  function moveGhost(ghostHistory) {
+
+    const ghost = ghostHistory[0]
 
     // console.log('ghost: ', ghost)
 
@@ -137,28 +158,23 @@ function setupGame() {
       // newGhost =  Math.floor(Math.random() * ghostOptions.length)
     }
 
-    ghostHistory = [ghostOptions[newGhost], ghost]
-    cells[ghost].classList.remove('ghost')
-    ghost = ghostOptions[newGhost]
-    cells[ghost].classList.add('ghost')
+    // check whether other ghost on new ghost cell
+    if (cells[ghostOptions[newGhost]].classList.contains('ghost')) {
+      ghostHistory = ghostHistory.reverse()
+    } else {
+      ghostHistory = [ghostOptions[newGhost], ghost]
+    }
+    cells[ghostHistory[1]].classList.remove('ghost')
+    cells[ghostHistory[0]].classList.add('ghost')
 
+    // check whether player on new ghost cell
+    if (cells[ghostHistory[0]].classList.contains('player')) {
+      ghostHistory = collideWithGhost(ghostHistory)
+    } 
     // console.log('Ghost history at end of move: ', ghostHistory)
 
-    // check conditions on new ghost cell
-    if (cells[ghost].classList.contains('player')) {
-      collideWithGhost()
-    }
-
+    return ghostHistory
   }
-
-  const ghostInterval = setInterval(() => {
-
-    console.log(frightened)
-    moveGhost()
-
-  }, 300)
-
-
 
   // PLAYER MOVEMENT
   function movePlayer(cellIndex) {
@@ -176,26 +192,48 @@ function setupGame() {
       if (newPosition.classList.contains('food')) {
         newPosition.classList.remove('food')
         score += 1
+        foodCount -= 1
         scoreCounter.innerHTML = score
 
-      } else if (newPosition.classList.contains('ghost') || ghostHistory.includes(player)) {
-        collideWithGhost()
+        if (foodCount === 0) {
+          endGame('win')
+        }
+
+      } else if (newPosition.classList.contains('ghost')) {
+        for (let i = 0; i < 4; i++) {
+          if (ghostHistories[i].includes(player)) {
+            ghostHistories[i] = collideWithGhost(ghostHistories[i])
+          }
+        }
+        
 
       } else if (newPosition.classList.contains('energizer')) {
         console.log('ENERGIZED')
         newPosition.classList.remove('energizer')
-        frightened = true
 
-        cells.forEach((cell) => {
-          cell.classList.add('frightened')
-        })
+        if (frightened === false ) {
+          frightened = true
 
-        setTimeout(() => {
-          frightened = false
           cells.forEach((cell) => {
-            cell.classList.remove('frightened')
+            cell.classList.add('frightened')
           })
-        }, 10000)
+
+          energizerTimeout = setTimeout(() => {
+            frightened = false
+            cells.forEach((cell) => {
+              cell.classList.remove('frightened')
+            })
+          }, 10000)
+
+        } else {       
+          clearTimeout(energizerTimeout)
+          energizerTimeout = setTimeout(() => {
+            frightened = false
+            cells.forEach((cell) => {
+              cell.classList.remove('frightened')
+            })
+          }, 10000)
+        }
 
       }
     }
@@ -234,6 +272,61 @@ function setupGame() {
       }
     }
   })
+
+
+  // GAME FUNCTION
+  function runGame() {
+
+    grid.removeChild(messageScreen)
+
+    score = 0
+    scoreCounter.innerHTML = score
+    lives = 3
+    lifeCounter.innerHTML = lives
+
+    player = 178
+    ghostHistories = [[22, 23], [40, 61], [400, 379], [418, 417]]
+    frightened = false
+    const board = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 3, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 4, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 4, 4, 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 4, 4, 4, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  
+    foodCount = 200
+    
+    // SET UP GAME BOARD
+    for (let i = 0; i < width ** 2; i++){
+      const cell = document.createElement('div')
+
+      const cellClass = board[i] 
+
+      switch (cellClass) {
+        case 1: cell.classList.add('wall'); break
+        case 2: cell.classList.add('food'); break
+        case 3: cell.classList.add('energizer'); break
+        case 4: cell.classList.add('ghostpen'); break
+      }
+
+      grid.appendChild(cell)
+      cells.push(cell)
+
+      // cell.innerHTML = i
+    }  
+
+    cells[player].classList.add('player')
+
+    for (let i = 0; i < 4; i++) {
+      const ghost = ghostHistories[i][0]
+      cells[ghost].classList.add('ghost')
+    }
+    
+    ghostInterval = setInterval(() => {
+
+      for (let i = 0; i < 4; i++) {
+        ghostHistories[i] = moveGhost(ghostHistories[i])
+      }
+
+    }, 400)
+
+  }
+   
 }
 
-document.addEventListener('DOMContentLoaded', setupGame)
+
+document.addEventListener('DOMContentLoaded', main)
